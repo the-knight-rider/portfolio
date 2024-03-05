003086c4b864063da1d13e0e7ed6451874ef4ed8
Aditya Shimpi <115714741+the-knight-rider@users.noreply.github.com>
2024-03-05 09:35:38 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './data/Pages/Home/HomeScreen';
+function App() {
+  return 
+    <div className="App">
+      <Home />
+     
+    </div>;
+}
+
+export default App;
---
+++ src/data/Pages/Home/HeroSection.jsx
@@ -0,0 +1,14 @@
+export default function HeroSection() {
+    return <section id="herosection" className="hero--section">
+       <div className="hero--section--content--box">
+        <div className="hero--section--content">
+            <p className="section--title">hey, I'm aditya </p>
+            <h1 className="hero--section--title">
+                <span className="hero--section--title--color">Full ssstack</span>{""}
+                <br/>
+                Devloper
+            </h1>
+        </div>
+       </div>
+        </section>;
+}
